a0a872c996dc308a48b101d933e88cce6a85aed9
Augusto Cesar <acs-03@hotmail.com>
2025-03-20 07:48:25 -0300

    feat: adiciona teste unitário na camada de service findAllShouldReturnPage

--- backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/services/impl/UsuarioServiceImplTests.java
+++ backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/services/impl/UsuarioServiceImplTests.java
@@ -5,19 +5,25 @@ import com.ecommerce.ms_usuario.factories.UsuarioFactory;
 import com.ecommerce.ms_usuario.models.UsuarioModel;
 import com.ecommerce.ms_usuario.repositories.UsuarioRepository;
 import com.ecommerce.ms_usuario.services.exceptions.ResourceNotFoundException;
+import com.ecommerce.ms_usuario.spacifications.queryFilters.UsuarioQueryFilter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -30,20 +36,22 @@ public class UsuarioServiceImplTests {
     UsuarioRepository usuarioRepository;
 
     private UsuarioDTO usuarioValidoDTO;
-    private UsuarioModel usuarioModel;
-
     private UUID existingId;
     private UUID notExistingId;
+    private UsuarioQueryFilter usuarioQueryFilter;
 
     @BeforeEach
     void setUp() throws Exception {
-        usuarioModel = UsuarioFactory.criarUsuarioModel();
+        UsuarioModel usuarioModel = UsuarioFactory.criarUsuarioModel();
         usuarioValidoDTO = UsuarioFactory.criarUsuarioValidoDTO();
         existingId = UUID.fromString("2ef38163-8438-43f2-847b-200e5e01b78f");
         notExistingId = UUID.fromString("3ef38163-8438-43f2-847b-200e5e01b78f");
+        usuarioQueryFilter = new UsuarioQueryFilter();
+        PageImpl<UsuarioModel> usuarioModelPage = new PageImpl<>(List.of(usuarioModel));
 
         when(usuarioRepository.save(any())).thenReturn(usuarioModel);
         when(usuarioRepository.findById(existingId)).thenReturn(Optional.of(usuarioModel));
+        when(usuarioRepository.findAll((Specification<UsuarioModel>) any(), (Pageable) any())).thenReturn(usuarioModelPage);
     }
 
     @Test
@@ -81,5 +89,14 @@ public class UsuarioServiceImplTests {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             usuarioService.findById(notExistingId);
         });
+    }
+
+    @Test
+    public void findAllShouldReturnPage() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<UsuarioModel> page = usuarioService.findAll(usuarioQueryFilter.toSpecification(), pageable);
+
+        Assertions.assertNotNull(page);
     }
 }
