558b546ff5ed5db688df0d5dac8c5802ff5476a0
Augusto Cesar <acs-03@hotmail.com>
2025-03-21 17:43:07 -0300

    feat: adiciona teste unitário na camada web createShouldReturnUnprocessableEntityWhenNameIsBlank

--- backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerTests.java
+++ backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerTests.java
@@ -89,4 +89,17 @@ public class UsuarioControllerTests {
                 .andExpect(jsonPath("$.errors[0].message")
                         .value("O nome deve ter entre 4 e 32 caracteres"));
     }
+
+    @Test
+    public void createShouldReturnUnprocessableEntityWhenNameIsBlank() throws Exception {
+        usuarioDTO.setNome("");
+        String jsonBody = objectMapper.writeValueAsString(usuarioDTO);
+
+        mockMvc.perform(post("/usuarios")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.errors[1].message")
+                        .value("O nome não pode estar em branco"));
+    }
 }
