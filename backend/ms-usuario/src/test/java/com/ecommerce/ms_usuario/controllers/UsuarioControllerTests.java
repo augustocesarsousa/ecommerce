f7e9597c28503287d7b01ba85aac18a15adac81a
Augusto Cesar <acs-03@hotmail.com>
2025-03-21 17:36:35 -0300

    feat: adiciona teste unitário na camada web createShouldReturnUnprocessableEntityWhenNameSizeIsThirtyThree

--- backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerTests.java
+++ backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerTests.java
@@ -76,4 +76,17 @@ public class UsuarioControllerTests {
                 .andExpect(jsonPath("$.errors[0].message")
                         .value("O nome deve ter entre 4 e 32 caracteres"));
     }
+
+    @Test
+    public void createShouldReturnUnprocessableEntityWhenNameSizeIsThirtyThree() throws Exception {
+        usuarioDTO.setNome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        String jsonBody = objectMapper.writeValueAsString(usuarioDTO);
+
+        mockMvc.perform(post("/usuarios")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.errors[0].message")
+                        .value("O nome deve ter entre 4 e 32 caracteres"));
+    }
 }
