0f47047e95ca18cc0284fc01900483ea74a84fc2
Augusto Cesar <acs-03@hotmail.com>
2025-03-22 17:55:27 -0300

    feat: adiciona teste unitário na camada web updateShouldReturnUnprocessableEntityWhenNameHasOnlyNumbers

--- backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerTests.java
+++ backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerTests.java
@@ -287,4 +287,17 @@ public class UsuarioControllerTests {
                         .value("Nome não informado"));
     }
 
+    @Test
+    public void updateShouldReturnUnprocessableEntityWhenNameHasOnlyNumbers() throws Exception {
+        usuarioDTO.setNome("1234");
+        String jsonBody = objectMapper.writeValueAsString(usuarioDTO);
+
+        mockMvc.perform(put("/usuarios/{id}", existingID)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.errors[0].message")
+                        .value("O nome não pode conter apenas números"));
+    }
+
 }
