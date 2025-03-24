90cef80384f535ef5ce13c1aeeb8682843be0422
Augusto Cesar <acs-03@hotmail.com>
2025-03-24 06:44:50 -0300

    feat: adiciona teste unitário na camada web, método get - findAllShouldReturnPage

--- backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerGetTests.java
+++ backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerGetTests.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,14 +34,19 @@ public class UsuarioControllerGetTests {
     private UUID existingUsuarioId;
     private UUID notExistingUsuarioId;
 
+    private PageImpl<UsuarioModel> page;
+
     @BeforeEach
     void setUp() throws Exception {
         usuarioModel = UsuarioFactory.criarUsuarioModel();
         existingUsuarioId = UUID.fromString("2ef38163-8438-43f2-847b-200e5e01b78f");
         notExistingUsuarioId = UUID.fromString("9ef38163-8438-43f2-847b-200e5e01b78f");
+        page = new PageImpl<>(List.of(usuarioModel));
 
         when(usuarioService.findById(existingUsuarioId)).thenReturn(usuarioModel);
         when(usuarioService.findById(notExistingUsuarioId)).thenThrow(ResourceNotFoundException.class);
+
+        when(usuarioService.findAll(any(), any())).thenReturn(page);
     }
 
     @Test
@@ -53,6 +61,12 @@ public class UsuarioControllerGetTests {
     public void findByIdShouldReturnNotFoundWhenIdDoesNotExisting() throws Exception {
         mockMvc.perform(get("/usuarios/{id}", notExistingUsuarioId))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+        mockMvc.perform(get("/usuarios"))
+                .andExpect(status().isOk());
     }
 
 }
