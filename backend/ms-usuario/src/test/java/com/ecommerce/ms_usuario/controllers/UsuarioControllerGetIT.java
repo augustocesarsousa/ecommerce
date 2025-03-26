9fedb22b5c9d945056af06a21c35929051c67a0d
Augusto Cesar <acs-03@hotmail.com>
2025-03-26 07:03:06 -0300

    feat: adiciona teste de integração na camada web - findByIdShouldReturnEntityFromDatabaseWhenExistingId

---
+++ backend/ms-usuario/src/test/java/com/ecommerce/ms_usuario/controllers/UsuarioControllerGetIT.java
@@ -0,0 +1,46 @@
+package com.ecommerce.ms_usuario.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class UsuarioControllerGetIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private UUID existingUsuarioId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingUsuarioId = UUID.fromString("2ef38163-8438-43f2-847b-200e5e01b78f");
+    }
+
+    @Test
+    public void findByIdShouldReturnEntityFromDatabaseWhenExistingId() throws Exception {
+        mockMvc.perform(get("/usuarios/{id}", existingUsuarioId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.nome").value("Bruce Wayne"))
+                .andExpect(jsonPath("$.telefone").value("11912345678"))
+                .andExpect(jsonPath("$.email").value("bruce.wayne@email.com"));
+    }
+}
