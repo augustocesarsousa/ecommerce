93b41aaaff27a38334af5e06bf542e174133d7fc
Augusto Cesar <acs-03@hotmail.com>
2025-07-10 07:21:19 -0300

    feat(ms-empresa): adiciona respostas customizadas para as validações da entidade Empresa

---
+++ backend/ms-empresa/src/main/java/com/ecommerce/ms_empresa/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,78 @@
+package com.ecommerce.ms_empresa.controllers.exceptions;
+
+import com.ecommerce.ms_empresa.services.exceptions.EntityAlreadyRegistered;
+import com.ecommerce.ms_empresa.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError();
+
+        standardError.setTimestamp(Instant.now());
+        standardError.setStatus(httpStatus.value());
+        standardError.setError("Resource not found");
+        standardError.setMessage(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(httpStatus).body(standardError);
+    }
+
+    @ExceptionHandler(EntityAlreadyRegistered.class)
+    public ResponseEntity<StandardError> entityAlreadyRegistered(EntityAlreadyRegistered e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.CONFLICT;
+        StandardError standardError = new StandardError();
+
+        standardError.setTimestamp(Instant.now());
+        standardError.setStatus(httpStatus.value());
+        standardError.setError("Entity already registered");
+        standardError.setMessage(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(httpStatus).body(standardError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validationContraints(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError validationError = new ValidationError();
+
+        validationError.setTimestamp(Instant.now());
+        validationError.setStatus(httpStatus.value());
+        validationError.setError("Validation exception");
+        validationError.setMessage(e.getMessage());
+        validationError.setPath(request.getRequestURI());
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()){
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(httpStatus).body(validationError);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> requestError(HttpMessageNotReadableException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
+        StandardError standardError = new StandardError();
+
+        standardError.setTimestamp(Instant.now());
+        standardError.setStatus(httpStatus.value());
+        standardError.setError("Request error");
+        standardError.setMessage(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(httpStatus).body(standardError);
+    }
+}
